a9a35ce05e3f6f3ceae2e4fc5a806a9430cf6534
AlejandroFuentesHernandez <41393694+AlejandroFuentesHernandez@users.noreply.github.com>
2018-09-03 16:51:30 -0600

    em
    ultimo del dia

--- application/views/datos_empleados_view.php
+++ application/views/datos_empleados_view.php
@@ -112,6 +112,59 @@
 						</div>
 						</div>
 						</div><!-- fin class container--->
+<!-- Modal-->
+<div>
+	<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledy="exampleModalLongTitle" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h1 class="modal-title" id="exampleModalLongTitle"> Tabla Empleados</h1>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+				</div>
+				<div class="modal-body">
+					<!--tabla para mostrar datos-->
+					<table border="solid">
+					<thead>
+						<tr>
+							<th>Numero del empleado</th>
+							<th>Nombre del empleado</th>
+							<th>DUI del empleado</th>
+							<th>Direccion del empleado</th>
+							<th>Fecha de nacimiento</th>
+							<th>Cargo del empleado</th>
+							<th>Telefono del empleado</th>
+							<th>Id de la tienda</th>
+							<th>Estado del usuario</th>
+							<!--
+								<th>borrar</th>
+								<th>editar</th>
+							-->
+						</tr>
+					</thead>
+					<tbody>
+						<?php ?>
+						<tr>
+							<!--<td><input type="radio" name="editar" value="<?=$u->idContacto?>"/></td>-->
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+							<td></td>
+						</tr>
 
+						<?php ?>
+					</tbody>
+				</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 </body>
 </html>
