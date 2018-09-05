4257ce7f19119cc4cb7c557732ee7f1300b9250b
AlejandroFuentesHernandez <41393694+AlejandroFuentesHernandez@users.noreply.github.com>
2018-09-05 14:55:56 -0600

    model
    usuario model

--- application/controllers/ingresar_empleadousua_controller.php
+++ application/controllers/ingresar_empleadousua_controller.php
@@ -10,108 +10,74 @@ class Ingresar_empleados_us extends CI_Model
 	public __construct()
 	{
 		parent::construct();
-		$this->load->model('ingresar_empleadousua_model');
+		$this->load->model('ingresar_empleadousua_model','empleadousua_model',TRUE);
 	}
 
 	public function inicio_datosempleado()
 	{
 		//mostrar datos en una vista
-		 $usuarios = $this->datos_empleado_model->getdataempleados();
-		$data['usuarios']=$usuarios;
-		$this->load->view('mostrar_datos_empleados',$data);
+		 $usuarios = $this->empleadousua_model->mostrar();
+		$data['dempleado']=$empleados;
+		$this->load->view('_datos_empleadousua',$data);
 	}
 
-	public function agregar_empleado()
+	public function registrar_empleado()
 	{
 
-
+		$numero_empleado=$this->input->post('nombre_empresa');
+		$nombre_empleado=$this->input->post('nombre_empleado');
+		$DUI_empleado=$this->input->post('DUI_empleado');
+		$direccion_empleados=$this->post('direccion_empleados');
+		$fecha_nacimiento=$this->post('fecha_nacimiento');
+		$cargo_empleado=$this->post('cargo_empleado');
+		$id_tienda=$this->post('id_tienda');
+		$estado_usuario=$this->post('estado_usuario');
 	//ingresar datos del post en variables $data
-	$data['numero_empleado']= $_POST['numero_empleado'];
+	/*$data['numero_empleado']= $_POST['numero_empleado'];
 	$data['nombre_empleado']= $_POST['nombre_empleado'];
 	$data['DUI_empleado']= $_POST['DUI_empleado'];
 	$data['direccion_empleados']= $_POST['direccion_empleados'];
 	$data['fecha_nacimiento']= $_POST['fecha_nacimiento'];
 	$data['cargo_empleado']= $_POST['cargo_empleado'];
 	$data['id_tienda']= $_POST['id_tienda'];
-	$data['estado_empleado']= $_POST['estado_empleado'];
+	$data['estado_empleado']= $_POST['estado_empleado'];*/
 
 
-	$this->datos_empleado_model->insertar_empleados($data);
-	//ingresar los datos en la funcion que inserta los datos para mostrarlos en otra vista
-	self::inicio_empleados();
-
-	}
-	public function eliminar_empleado()
+	$data=array
+	(
+		//'id_empleado' =>$id_empleado , 
+		'numero_empleado'=>$numero_empleado,
+		'nombre_empleado'=>$nombre_empresa,
+		'DUI_empleado'=>$DUI_empleado,
+		'direccion_empleados'=>$direccion_empleados,
+		'fecha_nacimiento'=>$fecha_nacimiento,
+		'cargo_empleado'=>$cargo_empleado,
+		'id_tienda'=>$estado_empleado,
+	);
+	$registro=$this->empleadousua_model->insertar($data);
+	$mensaje ['insertar']='Registro exitoso';
+	if($registro==1)
 	{
+		$ruta=base_url('datos_empleadousua_controller');
 
-		$nombre= $_POST['numero_empleado'];
-		$this->datos_empleado_model->deshabilitar_empleados($numero_empleado,$estado_usuario);
-
+		echo "<script>
+			alert('Empleado guardado satisfactoriamente.');
+			window.location='{ruta}';
+		</script>";
+		$this->load->view('_datos_empleadousua_view',$mensaje);
 	}
-	public function accion_empleados()
-	{
-	
-	$data['usuario']=$this->datos_empleados_model->obtener_empleados($_POST['editar_tab_empleados']);
-	$this->load->view('datos_empleado_view',$data);
-	}
-
-	public function editar_tab_empleados()
-	{
-
-
-		$data['numero_empleado']= $_POST['numero_empleado'];
-		$data['nombre_empleado']= $_POST['nombre_empleado'];
-		$data['DUI_empleado']= $_POST['DUI_empleado'];
-		$data['direccion_empleados']= $_POST['direccion_empleados'];
-		$data['fecha_nacimiento']= $_POST['fecha_nacimiento'];
-		$data['cargo_empleado']= $_POST['cargo_empleado'];
-		$data['id_empleado']= $_POST['id_empleado'];
-		$data['estado_empleado']= $_POST['estado_empleado'];
-		$this->datos_empleados_model->update($data);
-		$this->inicio_empleados();
+	else{
+		$this->error();
 	}
 
-	public function inicio_datosusuario()
-	{
-		//mostrar datos en una vista
-		$usuario=$this->datos_usuario_model->getdatausuario();
-		$data['usuario']=$usuario;
-		$this->load->view('mostrar_datos_usuario',$data);
 	}
-	public function agregar_usuario()
-	{
-		$data['nombre_usuario']= $_POST['nombre_usuario'];
-		$data['contraseña_usuario']= $_POST['contraseña_usuario'];
-		$data['rol_usuario']= $_POST['rol_usuario'];
-		$data['id_empleado']= $_POST['id_empleado'];
-		$data['estado_usuario']= $_POST['estado_usuario'];
-
-		$this->datos_usuario_model->insertar_usuarios($data);
-		//ingresar los datos en la funcion que inserta los datos para mostrarlos en otra vista
-		self::inicio_usuario();
-	}
-	public function eliminar_usuarios()
-	{
-		$nombre= $_POST['id_empleado'];
-		$this->datos_usuario_model->deshabilitar_usuarios($id_empleado,$estado_usuario);
-	}
-	public function accion_usuarios()
-	{
-		$data['usuario']=$this->datos_usuario_model->obtener_usuarios($_POST['editar_tab_usuarios']);
-	}
-	public function editar_tab_usuarios()
-	{
-		$data['nombre_usuario']= $_POST['nombre_usuario'];
-		$data['contraseña_usuario']= $_POST['contraseña_usuario'];
-		$data['rol_usuario']= $_POST['rol_usuario'];
-		$data['id_empleado']= $_POST['id_empleado'];
-		$data['estado_usuario']= $_POST['estado_usuario'];
-
-		$this->datos_usuario_model->update($data);
-		$this->inicio_usuario();
-
-	}
+public function mostrar_empleados()
+{
+	$empleados=$this->empleado->mostrar();
+	$data['dempleados']=$empleados;
+	$this->load->view('mostrar_empleadousu_view');
 }
 
+}//ultima llave
 
 ?>
--- application/models/ingresar_empleadousua_model.php
+++ application/models/ingresar_empleadousua_model.php
@@ -4,54 +4,29 @@ define('BASEPATH') OR 	exit('No direct script access allowed');
 /**
  * 
  */
+function __construct()
+{
+	$this->load->database();
+}
+
+
 class Ingresar_empleadousua_model extends CI_Model
 {
 	//los modelos no necesitan un constructor
-	public function getdatausuario()
-	{
-		//nombre de la tabla
-		$usuario = $this->db->get('tab_usuario');
-		return $usuario->result();
-	}
 
-	public function insertar_empleados($data)
-	{
-		//insertar datos
-		$this->db->set('nombre_usuario',$data['nombre_usuario']);
-		$this->db->set('contraseña_usuario',$data['contraseña_usuario']);
-		$this->db->set('rol_usuario',$data['rol_usuario']);
-		$this->db->set('id_empleado',$data['id_empleado']);
-		$this->db->set('estado_usuario',$data['estado_usuario']);
-		$this->db->insert('tab_usuario');
+	public function insertar($data){
+		$resultado=$this->db->insert('tab_empleados',$data);
+		if($resultado==true)
+		{
+			return 1;
+		}else
+		{
+			return 0;
+		}
 	}
-	public function deshabilitar_usuarios($id_empleado,$estado_usuario)
-	{
-		$this->db->set('estado_usuario',$data['estado_usuario']);
-		$this->db->where('id_empleado',$id_empleado);
-		$this->db->update('tab_usuario');
-	}
-	public function obtener_usuario($id_empleado)
-	{
-		//obtener usuarios en base numero empleado
-		$this->db->select('id_empleado','nombre_usuario','contraseña_usuario','rol_usuario','estado_usuario',);
-		$this->db->from('tab_usuario');
-		//iria id?
-		$this->db->where('id_empleado'.$id_empleado);
-		return $tab_usuario->row();
-
-	}
-	public function update($data)
-	{
-	//update
-	$this->db->set('nombre_usuario',$data['nombre_usuario']);
-	$this->db->set('contraseña_usuario',$data['contraseña_usuario']);
-	$this->db->set('rol_usuario',$data['rol_usuario']);
-	$this->db->set('id_empleado',$data['id_empleado']);
-	$this->db->set('estado_usuario',$data['estado_usuario']);
-
-	$this->db->where('id_usuario'),$data['id_usuario'];
-	$this->db->update('tab_usuario');
-
+	public function mostrar(){
+		$empleados=$this->bd->get('tab_empleados');
+		return $empleados->result();
 	}
 }
 
