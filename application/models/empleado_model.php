b3a9e308a4a8a580bb06ab7f04cdfd36f255e24a
AlejandroFuentesHernandez <41393694+AlejandroFuentesHernandez@users.noreply.github.com>
2018-09-11 09:36:55 -0600

    cambio model
    c

--- application/models/empleado_model.php
+++ application/models/empleado_model.php
@@ -9,7 +9,7 @@ class Empleado_model extends CI_Model{//Inicio de insercion
 	public function insertar($data)
 	{
 		//se insertan los datos
-		$x=$this->db->insert('',$data);
+		$x=$this->db->insert('tab_datos_empleado',$data);
 		if($x==true)
 		{
 			return 1;
@@ -22,7 +22,7 @@ class Empleado_model extends CI_Model{//Inicio de insercion
 	//Inicio de musetra de datos
 	public function mostrar('tab_empleado')
 	{
-		$empleado=$this->db->get('tab_empleado');//que es lo mismo que: SELECT* FROM tab_empleado
+		$empleado=$this->db->get('tab_datos_empleado');//que es lo mismo que: SELECT* FROM tab_empleado
 		return $empleado->result();
 	}//Fin de muestra de datos
 
