eb311baf5fa669f5e07bca6eebcff5ca74974700
AlejandroFuentesHernandez <41393694+AlejandroFuentesHernandez@users.noreply.github.com>
2018-09-18 11:01:01 -0600

    j
    tienda js

--- js/Script_tienda.js
+++ js/Script_tienda.js
@@ -1,1 +1,13 @@
-Script_tienda
+$('#exampleModalLong').on('shown.bs.modal',function(){
+	$('#myInput').trigger('focus');
+});
+
+function _vacio()
+{
+	var =document.getElementById("").value.trim();
+	if(=='')
+	{
+		alert('');
+		document.getElementById("").focus();
+	}
+}
