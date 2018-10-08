9ef50f68aacf5d1435a6392626e476a21f469f43
AlejandroFuentesHernandez <41393694+AlejandroFuentesHernandez@users.noreply.github.com>
2018-10-08 15:55:44 -0600

    y
    y

--- application/views/modificar_tienda.php
+++ application/views/modificar_tienda.php
@@ -53,50 +53,27 @@ require 'application/views/Plantilla/navbar.php';
 												<input type="text" id="telefono_tienda" name="telefono_tienda" class="form-control"></div>
 											</div>
 											<!--<?php }?>-->
-											<div class="col-md-12 form-group input-group" >
-												<label for="" class="input-group-addon"></label>
+											<!-- boton-->
+											<div class="col-md-12 text-center">
+											<button type="submit" class="btn btn-success">Guardar Tienda</button>
 											</div>
 										</form><!-- form body-->
 									</div><!-- panel-body-->
 								</div><!-- panel-primary-->
 							</div><!-- col-md-12-->
 						</div><!-- row-->
+							<div class="modal-footer">
+								<a href="<?php echo base_url();?>Tienda/modificar" class="btn btn-danger">Close</a>
+							</div>
 					</div><!-- modal-body-->
 				</div><!-- modal-content-->
 			</div><!-- modal-dialog-->
 		</div><!-- modal fade-->
 	</div><!-- page-wrapper-->
-	<div class="row" style="padding-left: 435px;">
-		<div class="col-md-6">
-			<h1>Actualizar</h1>
-		</div>
-	</div><!-- row padding-->
-	<!-- fila 2-->
-	<div class="row" style="padding-left: 435px;">
-		<!-- parte 2-->
-		<div class="col-md-6">
-			<div class="panel panel-primary">
-				<div class="panel panel-heading">Editar tienda</div>
-				<!-- cuerpo -->
-				<div class="panel-body">
-					<form id="formTienda" action="<?php echo site_url();?>" method="POST">
-						<!-- primer campo-->
-						<div class="col-md-12 form-group input-group">
-							<label for="" class="input-group-addon">Nombre tienda</label>
-							<input type="text" id="nombre_tienda" name="nombre_tienda" class="form-control"></div>
-						<!-- segundo campo-->
-							<div class="col-md-12 form-group input-group">
-							<label for="" class="input-group-addon">Direccion tienda</label>
-							<input type="text" id="direccion_tienda" name="direccion_tienda" class="form-control"></div>
-
-							<div class="col-md-12 form-group input-group">
-							<label for="" class="input-group-addon">Telefono_tienda</label>
-							<input type="text" id="telefono_tienda" name="telefono_tienda" class="form-control"></div>
-					</form><!-- form-->
-				</div><!-- panel-body-->
-			</div><!-- panel-primary-->
-		</div><!-- col-md-6-->
-	</div>
+	<!-- DataTables JavaScript-->
+	<script src="..vendor/datatables/js/jquery.dataTables.min.js"></script>
+	<script src="../vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
+	<script src="../vendor/dataTables-responsive/dataTables.dataTables-responsive.js"></script>
 
 </body>
 </html>
