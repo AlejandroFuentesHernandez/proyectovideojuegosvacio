876dafd48250d4097658b41064373ad3834b86e5
AlejandroFuentesHernandez <41393694+AlejandroFuentesHernandez@users.noreply.github.com>
2018-10-11 15:34:18 -0600

    n
    n

---
+++ application/views/mostrar_venta.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Mostrar ventas</title>
+</head>
+<body>
+	<!-- Inicio modal-->
+	<div class="modal fade" data-backdrop="static" id="mostrar_ventas" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+		<div class="modal-dialog" role="document"><!-- establecer el anchoy el margen del modal-->
+			<div class="modal-content">
+				<div class="modal-content">
+					<div class="modal-body">
+						<div class="row">
+							<div class="col-md-12">
+								<div class="panel panel-primary">
+									<div class="panel-heading">mostrar venta</div><!--panel-heading-->
+									<!--cuerpo-->
+									<div class="panel-body">
+										<form id="" action="">
+											
+										</form><!--form-->
+									</div><!--panel-body-->
+								</div><!--panel-primary-->
+							</div><!--col-md-12-->
+						</div><!-- row-->
+					</div><!-- modal-body-->
+				</div><!-- modal-content-->
+			</div><!-- modal-content-->
+		</div><!--modal-dialog-->
+	</div><!-- modal fade-->
+</body>
+</html>
