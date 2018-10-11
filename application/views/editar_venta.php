4d45890dc4c96d06c7436c16625f6080d5e25a62
AlejandroFuentesHernandez <41393694+AlejandroFuentesHernandez@users.noreply.github.com>
2018-10-11 16:22:37 -0600

    m
    m

---
+++ application/views/editar_venta.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Modificar venta</title>
+</head>
+<body>
+	<!-- Inicio modal en el se editara la venta-->
+	<div class="modal fade" data-backdrop="static" id="" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+		<div class="modal-dialog" role="document"><!--establecer ancho y el margen del modal-->
+			<div class="modal-content">
+				<div class="modal-body">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="panel panel-primary">
+								<div class="panel-heading">Editar ventas</div>
+								<!--panel-heading-->
+								<!--cuerpo-->
+								<div class="panel-body">
+									
+								</div><!--panel-body-->
+							</div><!--panel-primary-->
+						</div><!--col-md-12-->
+					</div><!--row-->
+				</div><!--modal-body-->
+			</div><!--modal-content-->
+		</div><!--modal-dialog-->
+	</div><!--modal fade-->
+</body>
+</html>
